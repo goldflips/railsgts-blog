791d1d7124c591f9d6abe4a2746f412630ba7020
goldflips <nekiracun75@gmail.com>
2016-06-29 15:54:24 +0000

    Added basic http auth and changed README

---
+++ README.md
@@ -0,0 +1,8 @@
+Ruby on Rails Workshop Tuzla
+============================
+
+Getting started guide
+---------------------
+
+My first Ruby on Rails App
+
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "amir", password: "secret", only: :destroy
 
   def index
     @articles = Article.all
